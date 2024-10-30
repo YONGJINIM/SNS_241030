bc8466e1ef604c6a1d1457cd0d780e7df338ddc2
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-10-30 21:40:43 +0900

    초기환경 세팅

---
+++ src/main/java/com/sns/test/TestController.java
@@ -0,0 +1,47 @@
+package com.sns.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.user.mapper.UserMapper;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	@ResponseBody
+    @RequestMapping("/test1")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+	
+	@ResponseBody
+	@GetMapping("/test2")
+	public Map<String, Object> test2(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("aaa", 121);
+		map.put("bbb", 242);
+		map.put("ccc", 353);
+		return map;
+	}
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+	@ResponseBody
+	@GetMapping("/test4")
+	public List<Map<String , Object>> test4(){ // 하나의 행은 Map에 담긴다
+		return userMapper.selectUserList();
+	}
+	
+}
+	
